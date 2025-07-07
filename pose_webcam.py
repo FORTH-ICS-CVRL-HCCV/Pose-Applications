d6313a89e6c4a9632a61d4c4c2b05ca776ad3e2d
nikosvasilik <vassilicopoulos@gmail.com>
2025-07-07 10:09:03 +0300

    Add webcam implementation for real-time pose detection

---
+++ pose_webcam.py
@@ -0,0 +1,33 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+import matplotlib.pyplot as plt
+#Run from webcam
+cap = cv2.VideoCapture(0)  # Change to filename for video input
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+mp_drawing_styles = mp.solutions.drawing_styles
+pose_detector = mp_pose.Pose(static_image_mode=False, model_complexity=2, enable_segmentation=True, min_detection_confidence=0.5)
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        print("Failed to capture video")
+        break
+    
+    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = pose_detector.process(image)
+    
+    if results.pose_landmarks:
+        mp_drawing.draw_landmarks(
+            image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+    
+    cv2.imshow('Pose Detection', cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
+    #Press 'q' to exit
+    if cv2.waitKey(5) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+

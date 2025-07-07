e7a8a16a075c0b5b0ce43908f8cd36ee991b77b0
MIhaliskar <pepperblackmk@gmail.com>
2025-07-07 22:06:41 +0300

    Added Framerate from Main

--- split_screen.py
+++ split_screen.py
@@ -2,15 +2,30 @@
 #pip install opencv-python mediapipe numpy matplotlib
 
 import cv2
+import time
 import mediapipe as mp
 import numpy as np
 import matplotlib.pyplot as plt
 #Run from webcam
+
+#Run from webcam
+videoWidth=None # Or 640 
+videoHeight=None  # Or 480
+
 cap = cv2.VideoCapture(0)  # Change to filename for video input
+
+if (videoHeight): 
+   cap.set(cv2.CAP_PROP_FRAME_WIDTH, videoWidth)
+if (videoWidth): 
+   cap.set(cv2.CAP_PROP_FRAME_HEIGHT, videoHeight)
+
+
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 mp_drawing_styles = mp.solutions.drawing_styles
 pose_detector = mp_pose.Pose(static_image_mode=False, model_complexity=2, enable_segmentation=True, min_detection_confidence=0.5)
+
+
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
@@ -28,8 +43,11 @@ while cap.isOpened():
     image1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2RGB)
     image2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2RGB)
 
+    start      = time.time()
     results1 = pose_detector.process(image1)
     results2 = pose_detector.process(image2)
+    seconds    = time.time() - start
+    hz    = 1 / (seconds+0.0001)
     
     if results1.pose_landmarks:
         mp_drawing.draw_landmarks(image1, results1.pose_landmarks, mp_pose.POSE_CONNECTIONS)
@@ -40,7 +58,9 @@ while cap.isOpened():
         mp_drawing.draw_landmarks(image2, results2.pose_landmarks, mp_pose.POSE_CONNECTIONS)
 
 
+    cv2.putText(image1, "Framerate: %0.2f Hz" % hz, (8,40),  cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 0), 1)
     cv2.imshow('Pose Detection1', cv2.cvtColor(image1, cv2.COLOR_RGB2BGR))
+    cv2.putText(image2, "Framerate: %0.2f Hz" % hz, (8,40),  cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 0), 1)
     cv2.imshow('Pose Detection2', cv2.cvtColor(image2, cv2.COLOR_RGB2BGR))
     
     #Press 'q' to exit

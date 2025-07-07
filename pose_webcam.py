170dbe8771df8891452e9bc5bd4f18c8e472e20e
Ammar Qammaz <ammarkov@gmail.com>
2025-07-07 10:36:45 +0300

    add deps in repo

--- pose_webcam.py
+++ pose_webcam.py
@@ -1,3 +1,6 @@
+#Dependencies : 
+#pip install opencv-python mediapipe numpy matplotlib
+
 import cv2
 import mediapipe as mp
 import numpy as np
@@ -18,12 +21,11 @@ while cap.isOpened():
     results = pose_detector.process(image)
     
     if results.pose_landmarks:
-        mp_drawing.draw_landmarks(
-            image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
     
     cv2.imshow('Pose Detection', cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
     #Press 'q' to exit
-    if cv2.waitKey(5) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cap.release()
 cv2.destroyAllWindows()

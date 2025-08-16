25591ea24e0228cd1987132ed67e4475c95a2610
MIhaliskar <pepperblackmk@gmail.com>
2025-08-16 22:12:18 +0300

    Added Button Class

--- move_square_improved.py
+++ move_square_improved.py
@@ -137,6 +137,38 @@ class Circle:
         else:
             self.center_y = finger_y - self.dis_y
 
+class Button:
+    def __init__(self, x, y, color, size, text):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.size = size
+        self. text = text
+
+    def Get_X(self):
+        return self.x
+
+    def Get_Y(self):
+        return self.y
+
+    def Get_Size(self):
+        return self.size
+
+    def Get_Text(self):
+        return self.text
+
+    def Set_X(self, x):
+        self.x = x
+
+    def Set_Y(self, y):
+        self.y = y    
+
+    def Set_Size(self, size):
+        self.size = size
+
+    def Set_Text(self, text):
+        self.text = text
+
 
 
 
@@ -187,6 +219,17 @@ def DetectCircleTouch(index_finger_tip, circle, img_h, img_w):
     distance = np.sqrt((finger_x - circle.Get_Center_X())**2 + (finger_y - circle.Get_Center_Y())**2)
     if (distance < circle.Get_Radius() + buffer):
         return True
+    
+    return False
+
+def DetectButton(index_finger_tip, rect, img_h, img_w):
+    buffer = 10
+    finger_x = int(index_finger_tip[0] * img_w)
+    finger_y = int(index_finger_tip[1] * img_h)
+
+    if (finger_x > rect.Get_X() - buffer and finger_x < rect.Get_X() + rect.Get_Size() + buffer):
+        if (finger_y > rect.Get_Y() - buffer and finger_y < rect.Get_Y() + rect.Get_Size() + buffer):
+            return True
     
     return False
 
